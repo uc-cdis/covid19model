33a94f12ac3fa036154480b2ab7a7bdc098c8496
mattgarvin1 <mattgarvin1@gmail.com>
2020-05-22 19:30:34 -0500

    begin convert Euro R to IL model

--- base.r
+++ base.r
@@ -4,6 +4,28 @@ library(lubridate)
 library(gdata)
 library(EnvStats)
 
+# for the python routine:
+# dataset = HierarchicalDataset(cases_dir="./ILCaseAndMortalityInputV1.csv",
+#                               interventions_dir="ILInterventions.csv",
+#                               ifr_dir="ILWeightedFatalityInput.csv",
+#                               serial_interval_dir="../../data/serial_interval.csv", 
+#                              )
+# ...
+# import pystan
+# sm = pystan.StanModel(file="../../stan-models/us_base.stan")
+#
+
+args = commandArgs(trailingOnly=TRUE)
+if(length(args) == 0) {
+  args = 'us_base'
+} 
+StanModel = args[1]
+print(sprintf("Running %s",StanModel))
+
+## Reading all data
+# td: read in IL case-mortality table
+d=readRDS('data/COVID-19-up-to-date.rds')
+
 # td: get country list from case-mortality matrix
 countries <- c(
   "Denmark",
@@ -19,29 +41,6 @@ countries <- c(
   "Switzerland"
 )
 
-# for the python routine:
-# dataset = HierarchicalDataset(cases_dir="./ILCaseAndMortalityInputV1.csv",
-#                               interventions_dir="ILInterventions.csv",
-#                               ifr_dir="ILWeightedFatalityInput.csv",
-#                               serial_interval_dir="../../data/serial_interval.csv", 
-#                              )
-# ...
-# import pystan
-# sm = pystan.StanModel(file="../../stan-models/us_base.stan")
-#
-
-args = commandArgs(trailingOnly=TRUE)
-if(length(args) == 0) {
-  # td: us_base
-  args = 'base'
-} 
-StanModel = args[1]
-print(sprintf("Running %s",StanModel))
-
-## Reading all data
-# td: read in IL case-mortality table
-d=readRDS('data/COVID-19-up-to-date.rds')
-
 ## get CFR
 # td: read in IL weighted_fatality
 cfr.by.country = read.csv("data/weighted_fatality.csv")
@@ -56,47 +55,44 @@ covariates = read.csv('data/interventions.csv', stringsAsFactors = FALSE)
 # td: probably remove this line
 covariates <- covariates[1:11, c(1,2,3,4,5,6, 7, 8)]
 
-## making all covariates that happen after lockdown to have same date as lockdown
-# td: cut this whole section
-covariates$schools_universities[covariates$schools_universities > covariates$lockdown] <- covariates$lockdown[covariates$schools_universities > covariates$lockdown]
-covariates$travel_restrictions[covariates$travel_restrictions > covariates$lockdown] <- covariates$lockdown[covariates$travel_restrictions > covariates$lockdown] 
-covariates$public_events[covariates$public_events > covariates$lockdown] <- covariates$lockdown[covariates$public_events > covariates$lockdown]
-covariates$sport[covariates$sport > covariates$lockdown] <- covariates$lockdown[covariates$sport > covariates$lockdown]
-covariates$social_distancing_encouraged[covariates$social_distancing_encouraged > covariates$lockdown] <- covariates$lockdown[covariates$social_distancing_encouraged > covariates$lockdown]
-covariates$self_isolating_if_ill[covariates$self_isolating_if_ill > covariates$lockdown] <- covariates$lockdown[covariates$self_isolating_if_ill > covariates$lockdown]
-
 # td: probably -2
 p <- ncol(covariates) - 1
 forecast = 0
 
-# td: remove DEBUG flow
-DEBUG = FALSE
-if(DEBUG == FALSE) {
-  N2 = 75 # Increase this for a further forecast
-}  else  {
-  ### For faster runs:
-  # countries = c("Austria","Belgium") #,Spain")
-  N2 = 75
-}
-# td: cut line
-# countries = c("Italy","United_Kingdom","Spain","Norway","Austria","Switzerland")
+# NOTE: 7 (6?) is the NUMBER OF COVARIATES in the original model -> see comment "icl: models should only take 6 covariates"
+# --> a hardcoded seven represents the number of covariates (i.e., interventions)
+# --> should be dynamic, just computed from the table of interventions
 
+# icl: Increase this for a further forecast
+N2 = 75 
 dates = list()
 reported_cases = list()
-# td: cut unused covariates; change lengthscale
-stan_data = list(M=length(countries),N=NULL,p=p,x1=poly(1:N2,2)[,1],x2=poly(1:N2,2)[,2],
-                 y=NULL,covariate1=NULL,covariate2=NULL,covariate3=NULL,covariate4=NULL,covariate5=NULL,covariate6=NULL,covariate7=NULL,deaths=NULL,f=NULL,
-                 N0=6,cases=NULL,LENGTHSCALE=7,SI=serial.interval$fit[1:N2],
-                 EpidemicStart = NULL) # N0 = 6 to make it consistent with Rayleigh
 deaths_by_country = list()
 
+stan_data = list(M=length(countries),
+                N=NULL,
+                p=p,
+                x1=poly(1:N2,2)[,1],
+                x2=poly(1:N2,2)[,2],
+                y=NULL,
+                covariate1=NULL, # -> lockdown -> presently the only intervention in the IL model 
+                deaths=NULL,
+                f=NULL,
+                N0=6, # icl: N0 = 6 to make it consistent with Rayleigh
+                cases=NULL,
+
+                # td: make this dynamic -> it's the number of covariates -> can be computed from the table of interventions
+                LENGTHSCALE=1,
+
+                SI=serial.interval$fit[1:N2],
+                EpidemicStart = NULL)
 
 for(Country in countries) {
 
   # should be fine
   CFR=cfr.by.country$weighted_fatality[cfr.by.country$country == Country]
   
-  # td: change 2:8
+  # td: change 2:8 to ???
   covariates1 <- covariates[covariates$Country == Country, 2:8]
   
   # td: countryCode, not Countries.and.territories
@@ -168,24 +164,16 @@ for(Country in countries) {
   cases=c(as.vector(as.numeric(d1$Cases)),rep(-1,forecast))
   deaths_by_country[[Country]] = as.vector(as.numeric(d1$Deaths))
   covariates2 <- as.data.frame(d1[, colnames(covariates1)])
-  # x=1:(N+forecast) # ?
+  # x=1:(N+forecast) # -> ?
   covariates2[N:(N+forecast),] <- covariates2[N,]
   
   ## icl: append data
   stan_data$N = c(stan_data$N,N)
   stan_data$y = c(stan_data$y,y[1]) # icl: just the index case!
-  # stan_data$x = cbind(stan_data$x,x) # ?
+  # stan_data$x = cbind(stan_data$x,x) # -> ?
 
-  # td: cut unused covariates
+  # should be fine
   stan_data$covariate1 = cbind(stan_data$covariate1,covariates2[,1])
-  stan_data$covariate2 = cbind(stan_data$covariate2,covariates2[,2])
-  stan_data$covariate3 = cbind(stan_data$covariate3,covariates2[,3])
-  stan_data$covariate4 = cbind(stan_data$covariate4,covariates2[,4])
-  stan_data$covariate5 = cbind(stan_data$covariate5,covariates2[,5])
-  stan_data$covariate6 = cbind(stan_data$covariate6,covariates2[,6])
-  stan_data$covariate7 = cbind(stan_data$covariate7,covariates2[,7]) 
-
-  # shouold be fine
   stan_data$f = cbind(stan_data$f,f)
   stan_data$deaths = cbind(stan_data$deaths,deaths)
   stan_data$cases = cbind(stan_data$cases,cases)
@@ -198,28 +186,12 @@ for(Country in countries) {
   }
 }
 
-# td: cut these
-stan_data$covariate2 = 0 * stan_data$covariate2 # remove travel bans
-stan_data$covariate4 = 0 * stan_data$covariate5 # remove sport
-#stan_data$covariate1 = stan_data$covariate1 # school closure
-stan_data$covariate2 = stan_data$covariate7 # self-isolating if ill
-#stan_data$covariate3 = stan_data$covariate3 # public events
-# create the `any intervention` covariate
-stan_data$covariate4 = 1*as.data.frame((stan_data$covariate1+
-                                          stan_data$covariate3+
-                                          stan_data$covariate5+
-                                          stan_data$covariate6+
-                                          stan_data$covariate7) >= 1)
-stan_data$covariate5 = stan_data$covariate5 # lockdown
-stan_data$covariate6 = stan_data$covariate6 # social distancing encouraged
-stan_data$covariate7 = 0 # models should only take 6 covariates
+# stan_data$covariate7 = 0 # icl: models should only take 6 covariates # -> ?
 
 # should be fine
 stan_data$y = t(stan_data$y)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
-
-# td: double check StanModel has right value - "us_base"
 m = stan_model(paste0('stan-models/',StanModel,'.stan'))
 
 # td: handle HMC convergence; see paper; consult with Phil.
@@ -248,17 +220,11 @@ save(fit,prediction,dates,reported_cases,deaths_by_country,countries,estimated.d
 #### now -> visualize model results -> ####
 
 # icl: to visualize results
-library(bayesplot)
-filename <- paste0('base-',JOBID)
-
-# td: cut all but lockdown
-plot_labels <- c("School Closure",
-                 "Self Isolation",
-                 "Public Events",
-                 "First Intervention",
-                 "Lockdown", 'Social distancing')
 
 # should be fine
+library(bayesplot)
+filename <- paste0('base-',JOBID)
+plot_labels <- c("Lockdown")
 alpha = (as.matrix(out$alpha))
 colnames(alpha) = plot_labels
 g = (mcmc_intervals(alpha, prob = .9))
@@ -266,11 +232,7 @@ ggsave(sprintf("results/%s-covars-alpha-log.pdf",filename),g,width=4,height=6)
 g = (mcmc_intervals(alpha, prob = .9,transformations = function(x) exp(-x)))
 ggsave(sprintf("results/%s-covars-alpha.pdf",filename),g,width=4,height=6)
 mu = (as.matrix(out$mu))
-
-# td: ensure countries extracted correctly from case-mortality matrix
 colnames(mu) = countries
-
-# should be fine
 g = (mcmc_intervals(mu,prob = .9))
 ggsave(sprintf("results/%s-covars-mu.pdf",filename),g,width=4,height=6)
 dimensions <- dim(out$Rt)
@@ -279,7 +241,7 @@ colnames(Rt) = countries
 g = (mcmc_intervals(Rt,prob = .9))
 ggsave(sprintf("results/%s-covars-final-rt.pdf",filename),g,width=4,height=6)
 
-# td: check and annotate these other R scripts.
 system(paste0("Rscript plot-3-panel.r ", filename,'.Rdata'))
+
 ## icl: to run this code you will need to adjust manual values of forecast required
 system(paste0("Rscript plot-forecast.r ",filename,'.Rdata')) 
