f991dd56bb77aba7b23e80f2feb29d1055274d7a
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-03 01:07:55 -0500

    close in on exploratory df

--- plot-explore.r
+++ plot-explore.r
@@ -3,33 +3,84 @@
 load("./results/run_2/us_base-488236.Rdata")
 
 # to wrangle:
-# R_t (okay)
-# R_0 (okay)
-# total rCases
-# total cases_l
-# total cases_0
-# total rDeaths
-# total deaths_l
-# total deaths_0
+# R_t (a)
+# R_0 (a)
+# total rCases (a)
+# total cases_l (a)
+# total cases_0 (a)
+# total rDeaths (a)
+# total deaths_l (a)
+# total deaths_0 (a)
 #
 # very simple.
 
-# 1. get Rt
-dimensions <- dim(out$Rt)
-Rt <- (as.matrix(out$Rt[,dimensions[2],]))
-meanRt <- as.data.frame.list(colMeans(Rt))
-colnames(meanRt) <- countries
+exploreNames <- c(
+    "County",
+    "Rt",
+    "R0",
+    "Modeled_Cases",
+    "Modeled_Cases_cf",
+    "Reported_Cases",
+    "Modeled_Deaths",
+    "Modeled_Deaths_cf",
+    "Reported_Deaths"
+)
 
-# 2. start build main exploratory dataframe
-library(reshape2)
-explore <- melt(as.matrix(meanRt), varnames=c("", "county"), value.name="Rt")[c("county", "Rt")]
+explore <- data.frame()
+colnames(explore) <- exploreNames
 
-# 3. get R0 (aka mu)
-mu = (as.matrix(out$mu))
-meanMu <- as.data.frame.list(colMeans(mu))
-colnames(meanMu) <- countries
-# bind to main df
-explore <- cbind(explore,R0=t(meanMu))
+for(i in 1:length(countries)){
 
-# 4. 
+    print("--- i ---")
+    print(i)
 
+    N <- length(dates[[i]])
+
+    # here! careful - country is an integer right here (is it? double check)
+    country <- countries[[i]]
+
+    print("--- N ---")
+    print(N)
+
+    # check
+    Rt <- mean(colMeans(out$Rt[,1:N,i]))
+    # check
+    R0 <- mean(colMeans(out$mu))
+
+    total_predicted_cases <- sum(colMeans(prediction[,1:N,i]))
+    total_predicted_cases_cf <- sum(colMeans(out$prediction0[,1:N,i]))
+    total_reported_cases <- sum(reported_cases[[i]])
+
+    total_estimated_deaths <- sum(colMeans(estimated.deaths[,1:N,i]))
+    total_estimated_deaths_cf <- sum(colMeans(estimated.deaths.cf[,1:N,i]))
+    total_reported_deaths <- sum(deaths_by_country[[i]])
+
+    # "County",
+    # "Rt",
+    # "R0",
+
+    # "Modeled_Cases",
+    # "Modeled_Cases_cf",
+    # "Reported_Cases",
+
+    # "Modeled_Deaths",
+    # "Modeled_Deaths_cf",
+    # "Reported_Deaths"
+
+    countyStats <- c(
+        country,
+        Rt,
+        R0,
+        total_predicted_cases,
+        total_predicted_cases_cf,
+        total_reported_cases,
+        total_estimated_deaths,
+        total_estimated_deaths_cf,
+        total_reported_deaths
+    )
+
+    explore <- rbind(explore, countyStats)
+}
+
+print("let's take a look")
+print(explore)
