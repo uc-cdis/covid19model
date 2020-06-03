a3ec4b8874ddf13f4f084f33fa88a66a62d82f4a
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-03 00:21:56 -0500

    collect R0

--- plot-explore.r
+++ plot-explore.r
@@ -4,7 +4,7 @@ load("./results/run_2/us_base-488236.Rdata")
 
 # to wrangle:
 # R_t (okay)
-# R_0
+# R_0 (okay)
 # total rCases
 # total cases_l
 # total cases_0
@@ -21,5 +21,15 @@ meanRt <- as.data.frame.list(colMeans(Rt))
 colnames(meanRt) <- countries
 
 # 2. start build main exploratory dataframe
+library(reshape2)
 explore <- melt(as.matrix(meanRt), varnames=c("", "county"), value.name="Rt")[c("county", "Rt")]
+
+# 3. get R0 (aka mu)
+mu = (as.matrix(out$mu))
+meanMu <- as.data.frame.list(colMeans(mu))
+colnames(meanMu) <- countries
+# bind to main df
+explore <- cbind(explore,R0=t(meanMu))
+
+# 4. 
 
