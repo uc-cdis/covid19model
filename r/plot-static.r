fbe73a1e55b7b771dc62187ae55cc420b8a37cf5
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-09 10:35:12 -0500

    add scratch space for improve viz

---
+++ r/plot-static.r
@@ -0,0 +1,8 @@
+# this code is for generating the final Rt and 3-panel plots
+# it's scratch space, for now
+# will get it pretty, then replace current viz. routine with this, and delete this file
+
+# immediate objective:
+# --> make nice the "final Rt" and "3-panel" images for the five_county_big sim
+# --> these images will be shared as static content on the commons site
+
