0624a7350d0623e0b3ba146229ab27bc5143df3c
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-21 12:14:19 -0500

    only visit data for IL

--- r/visit-data/get-visit-data.sh
+++ r/visit-data/get-visit-data.sh
@@ -3,19 +3,20 @@
 rm 'visit-data/visitdata-grouped.csv'
 touch 'visit-data/visitdata-grouped.csv'
 
-# states="Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada NewHampshire NewJersey NewMexico NewYork NorthCarolina NorthDakota Ohio Oklahoma Oregon Pennsylvania RhodeIsland SouthCarolina SouthDakota Tennessee Texas Utah Vermont Virginia Washington WestVirginia Wisconsin Wyoming"
-states="Illinois"
+# Get and keep header from Illinois
+echo "Illinois"
+wget -q "https://visitdata.org/data/groupedIllinois.csv" -O "visit-data/Illinois.csv"
+cat "visit-data/Illinois.csv" >> 'visit-data/visitdata-grouped.csv'
+rm "visit-data/Illinois.csv"
 
-# Get header from Alabama
-echo "Alabama"
-wget -q "https://visitdata.org/data/groupedAlabama.csv" -O "visit-data/Alabama.csv"
-cat "visit-data/Alabama.csv" >> 'visit-data/visitdata-grouped.csv'
-rm "visit-data/Alabama.csv"
-
-for state in $states;
-do
-    echo ${state}
-    wget -q "https://visitdata.org/data/grouped${state}.csv" -O "${state}.csv"
-    tail -n +2 "${state}.csv" >> 'visit-data/visitdata-grouped.csv'
-    rm "${state}.csv"
-done
+## for many states
+# states="Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada NewHampshire NewJersey NewMexico NewYork NorthCarolina NorthDakota Ohio Oklahoma Oregon Pennsylvania RhodeIsland SouthCarolina SouthDakota Tennessee Texas Utah Vermont Virginia Washington WestVirginia Wisconsin Wyoming"
+# for state in $states;
+# do
+#   if [ state != "Illinois" ]; then
+#       echo ${state}
+#       wget -q "https://visitdata.org/data/grouped${state}.csv" -O "${state}.csv"
+#       tail -n +2 "${state}.csv" >> 'visit-data/visitdata-grouped.csv'
+#       rm "${state}.csv"
+#   fi
+# done
