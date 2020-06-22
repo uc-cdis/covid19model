708a32357c6f821f66a8688d275451f7e5e04c23
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-21 22:23:30 -0500

    revise visitdata bash

--- modelInput/mobility/visit-data/get-visit-data.sh
+++ modelInput/mobility/visit-data/get-visit-data.sh
@@ -1,13 +1,13 @@
 # Grabs the groupedState files from https://visitdata.org/data and concatenates the files
 
-rm 'visit-data/visitdata-grouped.csv'
-touch 'visit-data/visitdata-grouped.csv'
+rm './visitdata-grouped.csv'
+touch './visitdata-grouped.csv'
 
-# Get and keep header from Illinois
+# Get all of and keep header from Illinois
 echo "Illinois"
-wget -q "https://visitdata.org/data/groupedIllinois.csv" -O "visit-data/Illinois.csv"
-cat "visit-data/Illinois.csv" >> 'visit-data/visitdata-grouped.csv'
-rm "visit-data/Illinois.csv"
+wget -q "https://visitdata.org/data/groupedIllinois.csv" -O "./Illinois.csv"
+cat "./Illinois.csv" >> './visitdata-grouped.csv'
+rm "./Illinois.csv"
 
 ## for many states
 # states="Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada NewHampshire NewJersey NewMexico NewYork NorthCarolina NorthDakota Ohio Oklahoma Oregon Pennsylvania RhodeIsland SouthCarolina SouthDakota Tennessee Texas Utah Vermont Virginia Washington WestVirginia Wisconsin Wyoming"
@@ -16,7 +16,7 @@ rm "visit-data/Illinois.csv"
 #   if [ state != "Illinois" ]; then
 #       echo ${state}
 #       wget -q "https://visitdata.org/data/grouped${state}.csv" -O "${state}.csv"
-#       tail -n +2 "${state}.csv" >> 'visit-data/visitdata-grouped.csv'
+#       tail -n +2 "${state}.csv" >> './visitdata-grouped.csv'
 #       rm "${state}.csv"
 #   fi
 # done
