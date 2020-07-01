318619c47882acd8c07b916cfdc4a16aa626d348
mattgarvin1 <mattgarvin1@gmail.com>
2020-07-01 13:23:42 -0500

    handle no validate flag

--- r/base.r
+++ r/base.r
@@ -19,7 +19,8 @@ StanModel = args[1]
 minimumReportedDeaths = as.integer(args[2])
 nStanIterations = as.integer(args[3])
 validateFlag = args[4]
-# fixme - handle flag not passed - validateFlag is NA
+if (is.na(validateFlag)){validateFlag <- ""}
+
 if (validateFlag == "--validate"){
   print("INFO: --validate flag passed - running validation routine")
 }
