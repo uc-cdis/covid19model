0ca08de89802927c160cd01c8525958639758121
mattgarvin1 <mattgarvin1@gmail.com>
2020-07-15 00:27:27 -0500

    make forecast zero

--- r/base.r
+++ r/base.r
@@ -81,8 +81,6 @@ cfr.by.country$country <-  sub("840", "", cfr.by.country$country) # cutoff US pr
 # serial interval discrete gamma distribution
 serial.interval = read.csv("../modelInput/ILSerialIntervalV1.csv") # new table
 
-# number of days to forecast beyond date of last observation
-forecast = 0
 # N2 is Number of time points with data plus forecast
 N2 = 0
 
@@ -166,7 +164,8 @@ for(Country in countries) {
   N = length(tmp$cases)  
   if(N2 - N < 0) {
     print(sprintf("raising N2 from %d to %d", N2, N))
-    N2 = N + 7
+    N2 = N
+    # N2 = N + 7
   }
 }
 
@@ -201,14 +200,7 @@ for(Country in countries) {
   N = length(d1$cases)
   print(sprintf("%s has %d days of data",Country,N))
   
-  # at least a seven day forecast 
-  # testing this
-  forecast <- max(N2 - N, 7)
-
-  # fix it at 7 -> uniform forecast across counties..
-  # can't do this - breaks routine as of now
-  # forecast doesn't even matter at this point
-  # forecast <- 7
+  short <- N2 - N
 
   # >>>>>>>>>>> mobility >>>>>>>>>>>>> #
 
@@ -220,7 +212,7 @@ for(Country in countries) {
   padded_covariates <- nu_pad_mobility(min_date, covariates_county, Country, N2)
 
   # include transit
-  transit_usage <- rep(1, (N + forecast))
+  transit_usage <- rep(1, (N + short))
 
   # creating features -> only want "partial_state"
   df_features <- create_features(padded_covariates, transit_usage)
@@ -229,7 +221,7 @@ for(Country in countries) {
 
   # <<<<<<<<<<< mobility <<<<<<<<<<<<< #
 
-  h = rep(0,forecast+N) # discrete hazard rate from time t = 1, ..., 100
+  h = rep(0,short+N) # discrete hazard rate from time t = 1, ..., 100
   mean1 = 5.1; cv1 = 0.86; # infection to onset
   mean2 = 18.8; cv2 = 0.45 # onset to death
 
@@ -253,10 +245,10 @@ for(Country in countries) {
 
   f = s * h
   
-  y=c(as.vector(as.numeric(d1$cases)),rep(-1,forecast))
+  y=c(as.vector(as.numeric(d1$cases)),rep(-1,short))
   reported_cases[[Country]] = as.vector(as.numeric(d1$cases))
-  deaths=c(as.vector(as.numeric(d1$deaths)),rep(-1,forecast))
-  cases=c(as.vector(as.numeric(d1$cases)),rep(-1,forecast))
+  deaths=c(as.vector(as.numeric(d1$deaths)),rep(-1,short))
+  cases=c(as.vector(as.numeric(d1$cases)),rep(-1,short))
   deaths_by_country[[Country]] = as.vector(as.numeric(d1$deaths))
 
   ## icl: append data
