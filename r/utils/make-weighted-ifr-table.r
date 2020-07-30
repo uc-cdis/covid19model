55b8d319c965cd73184f87407ac77b87cfe9e83a
mattgarvin1 <mattgarvin1@gmail.com>
2020-07-30 17:44:42 -0500

    subset ages to rel freq

--- r/utils/make-weighted-ifr-table.r
+++ r/utils/make-weighted-ifr-table.r
@@ -1,1 +1,20 @@
+convertCode <- function(code) {
+  s <- as.character(code)
+  short <- 5 - nchar(s)
+  out <- paste(c(rep("0",short),s), collapse="")
+  return(out)
+}
+
 ages <- read.csv("../../modelInput/AgeDistributions.csv")
+
+ages$countyCode <- sapply(ages$countyCode, convertCode)
+
+dropCols <- list()
+k <- 1
+for (n in names(ages)){
+    if (!grepl("percent", n, ignore.case=TRUE) & (n != "countyCode")) {
+        dropCols[k] <- n
+        k <- k + 1
+    }
+}
+freq <- ages[,!names(ages) %in% dropCols]
