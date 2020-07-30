a68bc7a5cc024f65e388c7b857834044d435f078
mattgarvin1 <mattgarvin1@gmail.com>
2020-07-30 14:18:38 -0500

    rename columns nicely

--- r/utils/process-age-dist.r
+++ r/utils/process-age-dist.r
@@ -7,10 +7,22 @@ raw <- read.csv("../modelInput/us-age-dist-by-county/data.csv", skip=1)
 dropCols <- list()
 k <- 1
 for (n in names(raw)) {
-    if (grepl("male", n, ignore.case=TRUE) or grepl("error", n, ignore.case=TRUE)) {
-        dropCols[[k]] <- n
-        k <- k + 1
+    if (grepl("male", n, ignore.case=TRUE) | 
+        grepl("error", n, ignore.case=TRUE) |
+        grepl("selected", n, ignore.case=TRUE) |
+        grepl("summary", n, ignore.case=TRUE) |
+        grepl("geographic", n, ignore.case=TRUE) |
+        grepl("allocated", n, ignore.case=TRUE) |
+        (n == "Estimate..Percent..Total.population")) {
+            dropCols[[k]] <- n
+            k <- k + 1
     }
 }
 
-data <- subset(raw, select = -dropCols)
+data <- raw[, !(names(raw) %in% dropCols)]
+
+# note: last 5 digits of "id" col is the census FIPS county code
+
+colnames(data) <- sub("Estimate..Total..Total.population..AGE..", "total.", colnames(data))
+colnames(data) <- sub("Estimate..Percent..Total.population..AGE..", "percent.", colnames(data))
+colnames(data) <- sub("Estimate..Total..Total.population", "population", colnames(data))
