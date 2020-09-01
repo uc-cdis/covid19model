82112a2619a472f0010d211fce7511dea11cd58d
mattgarvin1 <mattgarvin1@gmail.com>
2020-09-01 16:05:34 -0500

    udpate bash scripts with new cli

--- run.sh
+++ run.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 # call form:
-# sh run.sh <stan_model> <minimumDeaths> <nIterations>
+# sh run.sh <stan_model> \
+#   <minimumDeaths> \
+#   <nIterations> \
+#   (-stateList | -batch) \
+#   (<stateList> | <batchID>) \
+#   [--validate]
 
-# example call:
-# sh run.sh us_base 150 200 [--validate]
+# example calls:
+## sh run.sh us_mobility 150 200 -stateList "Illinois,NewYork"
+## sh run.sh us_mobility 150 200 -stateList "Illinois,NewYork" --validate
+## sh run.sh us_mobility 150 200 -stateList "all"
+## sh run.sh us_mobility 150 200 -batch 1
 
 echo "\n--- running input ETL and model with these parameters ---"
-echo 'stanModel = ' $1
-echo 'minimumDeaths = ' $2
-echo 'mcmcIterations = ' $3
-echo 'validationFlag = ' $4
+echo 'stanModel = '         $1
+echo 'minimumDeaths = '     $2
+echo 'mcmcIterations = '    $3
+echo 'countySelector = '    $4
+echo 'selectorValue = '     $5
+echo 'validationFlag = '    $6
 
 # run the etl to generate all input tables
 echo "\n- Input ETL -"
@@ -29,7 +39,7 @@ Rscript mobility-regression.r > /dev/null 2>&1
 # run the model via R script
 echo "\n- Model Run -"
 # cd ../r
-Rscript base.r $1 $2 $3 $4
+Rscript base.r $1 $2 $3 $4 $5 $6
 
 cd ..
 
