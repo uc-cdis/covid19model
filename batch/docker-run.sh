02e584263473eeba43683966b04a0d10b1f1ec2d
mattgarvin1 <mattgarvin1@gmail.com>
2020-09-02 13:31:07 -0500

    fix batch bash

--- batch/docker-run.sh
+++ batch/docker-run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-### example call for this script
-# sh docker-run.sh -stateList "Illinois,NewYork" -deathsCutoff 100 -maxBatchSize 35
-
 echo "\n--- making county batches with these parameters ---"
-echo 'stateList = '     $2
-echo 'deathsCutoff = '  $4
-echo 'maxBatchSize = '  $6
+echo 'stateList = '     $STATE_LIST
+echo 'deathsCutoff = '  $DEATHS_CUTOFF
+echo 'maxBatchSize = '  $MAX_BATCH_SIZE
 
 #### example calls to Rscript
 # Rscript make-batches.r -stateList "Illinois,NewYork" -deathsCutoff 100 -maxBatchSize 35
 # Rscript make-batches.r -stateList "all" -deathsCutoff 100 -maxBatchSize 35
 
 # run the Rscript
-Rscript make-batches.r -stateList $2 -deathsCutoff $4 -maxBatchSize $6
+Rscript make-batches.r -stateList $STATE_LIST -deathsCutoff $DEATHS_CUTOFF -maxBatchSize $MAX_BATCH_SIZE
 
 echo "\n--- routine completed ---\n"
